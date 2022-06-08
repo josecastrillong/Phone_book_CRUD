96c93b94fc70c6b8b38a9fbe74214e0ae0ab0d31
jose_miguel <josecastrillong@gmail.com>
2022-06-08 18:14:41 -0500

    fixed errors in delete and post methods, implemented try-catch statement for all methods for error handling

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(morgan(':method :url :status :body - :response-time ms'))
 
 
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -40,37 +40,41 @@ app.get('/info', (request, res) => {
     res.send(`<h1>Phone book has info for ${persons.length}</h1><p>${new Date()}</p>`);
 });
 
-
-// GET API/TASKS
 app.get('/api/persons', (req, res) => {
   res.json(persons);
-})
+});
 
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id;
   const phone = persons.find(task => task.id === Number(id));
-  
+
+  try {
     if (!phone) {
-        res.status(404).json({ message: `Phone not found with id: ${id}` });
-    } else {
-        res.json(phone);
-      }
-})
-
-
-app.delete('/api/persons/:id',(req, res)=>{
-    const id = req.params.id;
-    const person = persons.find((person)=>{
-      return person.id === Number(id);
-  })
-
-    if(!person){
-      res.status(404).json({Message: "not found"})
+      throw new Error(`Person with id ${id} not found`);
     }
-    else{res.json(person)}
-  })
+    res.status(200).json(phone);
+  } catch (error) {
+    res.status(400).json( `ERROR: ${error}` );
+  }
+});
 
-  app.post('/api/persons',(req, res)=>{
+
+app.delete('/api/persons-delete/:id',(req, res)=>{
+    const id = req.params.id;
+    let person = persons.find(person => person.id === Number(id));
+
+    try {
+      if (!person) {
+        throw new Error(`Person with id ${id} not found`);
+      }
+      persons = persons.filter(person => person.id !== Number(id));
+      res.status(200).json(`Person with id ${id} deleted`);
+    } catch (error) {
+      res.status(400).json( `ERROR: ${error}` );
+    }
+});
+
+  app.post('/api/persons-post',(req, res)=>{
     const id = Math.round(Math.random()*10000)
     const newPerson = {id, ...req.body}
 
@@ -78,17 +82,19 @@ app.delete('/api/persons/:id',(req, res)=>{
       if(newPerson.name.length === 0 || newPerson.number.length === 0){
         res.status(400).json({Message: "entrada no valida"})
         throw new SyntaxError("incomplete data");
-  
-      }else{
+      }else if(persons.find(person => person.name === newPerson.name)){
+        res.status(400).json({Message: "name already exists"})
+        throw new Error("name already exists");
+      }
+      else{
         console.log(newPerson)
         persons.push(newPerson)
         res.status(201).json({Message: "Contacto aregado"})
       }    
     } catch(e){
-      console.error(`[ERROR]: this is the error ${e}`)
+      console.error(`ERROR: this is the error ${e}`)
     }
-
-    })  
+  });  
     
 const port = process.env.PORT || 3001;
 app.listen(port);
